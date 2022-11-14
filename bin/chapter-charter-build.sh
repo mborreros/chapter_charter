c9c1c3903aba2949e2c41492495014522b8a1b07
mborreros <mlbsindia@gmail.com>
2022-11-14 15:26:13 -0500

    attempt to correct build file

--- bin/chapter-charter-build.sh
+++ bin/chapter-charter-build.sh
@@ -1,25 +1,25 @@
-# from tutorial through canvas
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
-
-# bundle install
-# # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
-# # bundle exec rake assets:clean
-# bundle exec rake db:migrate 
-# # bundle exec rake db:seed
-
-# from tutorial through how to deploy react with rails
+from tutorial through canvas
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-# clean
-rm -rf public
-# build
-npm install --prefix client && npm run build --prefix client
-# migrate
-bundle exec rake db:migrate
-# postbuild
-cp -a client/build/. public/
+# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
+# bundle exec rake assets:clean
+bundle exec rake db:migrate 
+# bundle exec rake db:seed
+
+# # from tutorial through how to deploy react with rails
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# bundle install
+# # clean
+# rm -rf public
+# # build
+# npm install --prefix client && npm run build --prefix client
+# # migrate
+# bundle exec rake db:migrate
+# # postbuild
+# cp -a client/build/. public/
