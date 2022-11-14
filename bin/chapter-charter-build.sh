ea0b54f8cb5dcbdf012dc8fdc4a3894291ba936a
mborreros <mlbsindia@gmail.com>
2022-11-14 15:50:16 -0500

    added build scripts to package json for later deployment

--- bin/chapter-charter-build.sh
+++ bin/chapter-charter-build.sh
@@ -1,13 +1,25 @@
-from tutorial through canvas
+# from tutorial through canvas
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# bundle install
+# # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
+# # bundle exec rake assets:clean
+# bundle exec rake db:migrate 
+# # bundle exec rake db:seed
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle install
-# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
-# bundle exec rake assets:clean
 bundle exec rake db:migrate 
-# bundle exec rake db:seed
 
 # # from tutorial through how to deploy react with rails
 # #!/usr/bin/env bash
