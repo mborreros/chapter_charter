0d4bd1e683f29e512eadb240a10033cf15c13664
mborreros <mlbsindia@gmail.com>
2022-11-14 14:40:05 -0500

    added additional bird test database feature (show)

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,5 +5,13 @@ class BirdsController < ApplicationController
     birds = Bird.all
     render json: birds
   end
-  
+
+  # SHOW /birds/:id
+  def show
+    bird = Bird.find(params[:id])
+    render json: bird
+  rescue ActiveRecord::RecordNotFound
+    render json: "Bird not found", status: :not_found
+  end
+
 end
--- bin/birds-build.sh
+++ bin/birds-build.sh
@@ -6,4 +6,4 @@ bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
 bundle exec rake db:migrate 
-bundle exec rake db:seed
+# bundle exec rake db:seed
