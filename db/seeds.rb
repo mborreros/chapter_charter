90b38edec904916aeb3db1f9b8196174d3292e7d
mborreros <mlbsindia@gmail.com>
2022-11-15 12:53:13 -0500

    updated seeds to remove test bird data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,3 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# seed data to test rails backend on render service
-Bird.create!(name: 'Black-Capped Chickadee', species: 'Poecile Atricapillus')
-Bird.create!(name: 'Grackle', species: 'Quiscalus Quiscula')
-Bird.create!(name: 'Common Starling', species: 'Sturnus Vulgaris')
-Bird.create!(name: 'Mourning Dove', species: 'Zenaida Macroura')
