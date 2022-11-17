129342935f1d1f0b7bc9738c1ce1dc9b6f8f4c67
mborreros <mlbsindia@gmail.com>
2022-11-17 15:34:35 -0500

    correcting journeys table for render build

--- db/migrate/20221117191804_add_completed_to_journey.rb
+++ db/migrate/20221117191804_add_completed_to_journey.rb
@@ -1,5 +1,8 @@
 class AddCompletedToJourney < ActiveRecord::Migration[7.0]
   def change
+    remove_column :journeys, :manually_completed
+
     add_column :journeys, :completed, :boolean
+    add_column :journeys, :manually_completed, :boolean
   end
 end
