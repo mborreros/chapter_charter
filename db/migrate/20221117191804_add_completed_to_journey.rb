9458a6dd16906bdfbeae8b6dcbc27ba5e64d89b1
mborreros <mlbsindia@gmail.com>
2022-11-17 15:37:23 -0500

    removed previous changes

--- db/migrate/20221117191804_add_completed_to_journey.rb
+++ db/migrate/20221117191804_add_completed_to_journey.rb
@@ -1,8 +1,5 @@
 class AddCompletedToJourney < ActiveRecord::Migration[7.0]
   def change
-    remove_column :journeys, :manually_completed
-
     add_column :journeys, :completed, :boolean
-    add_column :journeys, :manually_completed, :boolean
   end
 end
