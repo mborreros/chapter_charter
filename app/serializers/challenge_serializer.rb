969681539f0fdd145b9f285413eab0e967fd4f0d
mborreros <mlbsindia@gmail.com>
2022-11-22 12:21:54 -0500

    update jrny valid, challenge serial for progress

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -1,7 +1,12 @@
 class ChallengeSerializer < ActiveModel::Serializer
 
-  attributes :id, :name, :description, :start_date, :end_date, :goal_number, :goal_type, :category, :category_identifier, :active, :successful
+  attributes :id, :name, :description, :start_date, :end_date, :goal_number, :goal_type, :category, :category_identifier, :challenge_progress, :active, :successful
 
   belongs_to :user
+
+  # shows current progress on challenge based on how many challenge entries
+  def challenge_progress
+    self.object.challenge_entries.count/self.object.goal_number.to_f*100
+  end
 
 end
