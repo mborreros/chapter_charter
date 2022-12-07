83cc44ed141ea5b2f8f43247cd71ed0f61493029
mborreros <mlbsindia@gmail.com>
2022-12-07 12:39:51 -0500

    added react-async-select for book search on jrny

---
+++ client/src/book_select_search.js
@@ -0,0 +1,85 @@
+import { useState, React, useCallback } from "react";
+import AsyncSelect from 'react-select/async'
+
+import { debounce } from "lodash"
+
+function BookSearch({ selectedValue, setSelectedValue }) {
+
+  const [inputValue, setValue] = useState('');
+
+    // handle input change event
+    const handleInputChange = value => {
+      setValue(value);
+    };
+
+    // handle selection
+    const handleChange = value => {
+      // setSelectedValue(value.key.replace('/works/',''));
+      setSelectedValue(value);
+    }
+
+    // fetching options from books API to display on user search
+    // utilizes debounce to minimize the number of fetchs regardless of user input characters
+    const loadOptions = useCallback(
+      debounce((inputValue, callback) => {
+        fetch(`https://openlibrary.org/search.json?q=${inputValue}`)
+        .then(res => res.json())
+        .then(data => callback(data.docs))  
+    }, 500),
+    []
+    );
+
+    // styling the options that appear on user input into search bar
+    function formatOptionLabel( option ) {
+      let pagesPublished = []
+      if (option.first_publish_year) {pagesPublished.push(`${option.first_publish_year}`)}
+      if (option.number_of_pages_median) {pagesPublished.push(`${option.number_of_pages_median} pages`)}
+
+      return(
+        <div className="row">
+
+        <div className="book-search-cover">
+          { option.cover_i ? 
+          <div style={{backgroundImage : `url("https://covers.openlibrary.org/b/id/${option.cover_i}-S.jpg")`}}></div> : 
+          <div style={{backgroundColor: "grey"}}></div>
+          }
+        </div>
+
+        <div className="col-5">
+          <div className="fw-bold fs-3">{option.title}</div>
+          <div className="opacity-75">{option.author_name ? option.author_name[0] : option.author_name}</div>
+        </div>
+        <div className="col-6 d-flex flex-column text-end justify-content-end text-lowercase">
+          <div className="opacity-50 text-truncate" title={option.subject?.slice(0,4).join(" / ")}>{option.subject?.slice(0,4).join(" / ")}</div>
+          <div className="opacity-50">{pagesPublished.join(' | ')}</div>
+        </div>
+      </div>
+      )
+    }
+
+    const determineLabel = (option, context) => {
+      if( context.context === 'menu' ){ return formatOptionLabel(option)} 
+      // styling of what is in the search box on user select
+      else { return (`Selected book: ${option.title} by ${option.author_name}`); }
+    }
+
+    return(
+      <div>
+        <AsyncSelect
+        value={selectedValue || ""}
+        getOptionLabel={option => option.title}
+        getOptionValue={option => option}
+        formatOptionLabel={determineLabel}
+        loadOptions={loadOptions}
+        hideSelectedOptions={false}
+        onInputChange={handleInputChange}
+        onChange={handleChange}
+        />
+        {/* allows to visualize selected value on user click, to be deleted once functionality is finsihed */}
+        <pre>Selected Value: {JSON.stringify(selectedValue || {}, null, 2)}</pre>
+      </div>
+    )
+}
+
+export default BookSearch;
+
--- client/src/journey_modal.js
+++ client/src/journey_modal.js
@@ -1,56 +1,16 @@
 import { useState } from "react";
-
-// import Select from 'react-select'
-// import Async, { useAsync } from 'react-select/async';
-// import AsyncSelect from 'react-select/async'
-import Button from "react-bootstrap/esm/Button";
 import Modal from 'react-bootstrap/Modal'
+
+import BookSearch from "./book_select_search";
 
 function JourneyModal({ show, handleClose, user }) {
 
-  const [query, setQuery] = useState("");
-  const [searchedBooks, setSearchedBooks] = useState([])
+  const [selectedValue, setSelectedValue] = useState(null);
 
-  function searchLibrary(e) {
-    e.preventDefault()
-    console.log("the user clicked to search the library with query:"+ query)
-    fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(query)}`)
-    .then((response) => response.json())
-    .then((books) => setSearchedBooks(books.docs))
-  }
-
-
-  let searchResults
-  if (searchedBooks) {
-    searchResults = searchedBooks?.map((bookResult) => {
-
-      let genres = []
-      if (bookResult.subject){ 
-        bookResult.subject.map((genre, index) => {
-          // takes only the first 5 genres listed
-          if (index <5) {
-            genres.push(genre)
-        } return genres
-      })}
-
-      return(
-      <div className="card search-book-card mb-5" key={bookResult.key}>
-        <div className="card-header justify-content-start align-items-start my-6">
-          {bookResult.isbn ? <img className="book-image-search-result"src={`https://covers.openlibrary.org/b/isbn/${bookResult.isbn[0]}-M.jpg`} /> : null}
-          <div className="card-title flex-column align-items-start justify-content-start mx-8">
-            {bookResult.title ? <span className="fs-3 fw-bold">{bookResult.title}</span> : null}
-            {bookResult.author_name ? <span className="fs-5">{bookResult.author_name[0]}</span> : null}
-            {bookResult.subject ? <span className="text-grey opacity-50 pt-1 fw-semibold fs-6">{genres.join(" ")}</span> : null}
-            {bookResult.number_of_pages_median && bookResult.first_publish_year ? <span className="text-grey opacity-50 pt-1 fw-semibold fs-6">{bookResult.number_of_pages_median} pages | first published in {bookResult.first_publish_year}</span> : <span className="text-grey opacity-50 pt-1 fw-semibold fs-6">first published in {bookResult.first_publish_year}</span>}
-
-              <Button className="start-journey-button" variant="secondary" size="sm" onClick={(bookResult) => console.log(bookResult)}>start this journey</Button>
-
-          </div>
-        </div>
-      </div>
-      )
-    }
-    )
+  function resetModalForm(form) {
+    // reset(form)
+    document.getElementById("book-searh-form").reset()
+    setSelectedValue("")
   }
 
   return(
@@ -60,7 +20,7 @@ function JourneyModal({ show, handleClose, user }) {
 					<Modal.Body>
 	
 							{/* start form */}
-							<form id="kt_modal_new_target_form" className="form" action="submit" autoComplete="off">
+							<form id="book-searh-form" className="form" action="submit" autoComplete="off">
 	
 								{/* start header */}
 								<Modal.Header className="border-0 justify-content-center flex-wrap">
@@ -69,35 +29,21 @@ function JourneyModal({ show, handleClose, user }) {
 								</Modal.Header>
 								{/* end header */}
 	
-								{/* start collection name input group */}
 								<div className="d-flex flex-column mt-8 mb-8 fv-row">
-									{/* start label */}
-									{/* <label className="d-flex align-items-center fs-6 fw-semibold mb-2">
-										<span className="required">Search Title or Author</span>
-										<i className="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="library_search"></i>
-									</label> */}
-                  <div className="d-flex">
-                    <input type="text" className="form-control form-control-solid" placeholder="Search the Library using a Author name or Book title" name="name" onChange={(e) => setQuery(e.target.value)}/>
-                    <button type="submit" id="kt_modal_new_target_submit" className="text-end btn btn-light btn-sm me-3" onClick={(e) => searchLibrary(e)}>Search</button>
-                  </div>
 
-									{/* end label */}
-									{/* <AsyncSelect loadOptions={loadOptions} onInputChange={(value) => setQuery(value)}/> */}
+                  <BookSearch selectedValue={selectedValue} setSelectedValue={setSelectedValue}/>
+
 								</div>
-								{/* end collection name input group */}
-                <div className="book-results-list">
-                  {searchResults}
-                </div>
 
 								{/* start action buttons */}
-								{/* <div className="text-end">
-									<button type="reset" id="kt_modal_new_target_cancel" className="btn btn-light btn-sm me-3">Clear</button>
+								<div className="text-end">
+									<button type="reset" id="kt_modal_new_target_cancel" className="btn btn-light btn-sm me-3" onClick={(form) => resetModalForm(form)}>Clear</button>
 									<button type="submit" id="kt_modal_new_target_submit" className="btn btn-primary btn-sm">
 										<span className="indicator-label">Submit</span>
 										<span className="indicator-progress">Please wait...
 										<span className="spinner-border spinner-border-sm align-middle ms-2"></span></span>
 									</button>
-								</div> */}
+								</div>
 								{/* end action buttons */}
 	
 							</form>
