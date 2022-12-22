0e2b385bac658d46e6d663adadbd4cf096e47455
mborreros <mlbsindia@gmail.com>
2022-12-22 18:09:18 -0500

    cleaned up css files

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import './bundle.css';
 import './styles.css';
 import './index.css';
 
